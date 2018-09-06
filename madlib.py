fd609fc6f040a624ea32bce5fbe5b33ffc5b1db7
Jeremy Harrelson <jeremy.harrelson1@marist.edu>
2018-09-06 15:01:36 -0400

    adding new file madlib.py

---
+++ madlib.py
@@ -0,0 +1,15 @@
+#CMPT 120L 113
+# MadLibs Program
+# Author: Jeremy Harrelson
+# Created 9/6/2018
+
+def main():
+    noun = input("Please enter a noun. ")
+    verb = input("\nPlease enter a verb. ")
+    adjective = input("\nPlease enter an adjective. ")
+    place = input("\nPlease enter a place. ")
+    
+    print("Take your ",adjective," ",noun," and ",verb," it at the ",place,".")
+    
+    
+main()
