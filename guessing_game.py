96c977b1522abc132515aa508bdac5542f654629
Jeremy Harrelson <jeremy.harrelson1@marist.edu>
2018-10-18 19:02:06 -0400

    Usability improvements to the guessing game, resolves #M and #N..

--- guessing_game.py
+++ guessing_game.py
@@ -6,14 +6,17 @@ def main():
     
     animal = "python"
     answer = False
-    while(not answer):
+    while(not answer or guess == q):
         guess = input("I am thinking of an animal.  "
-        "What is it?  ")
-        guess.lower().strip()
+        "What is it?  ").lower().strip()
+        print("your guess is:",guess)
         if(guess == animal):
             answer = True
             print("Lucky guess!  Then again this game IS"
                   " called \nthe PYTHON guessing game.")
+        elif(guess == 'q' or guess == 'quit'):
+            print("Giving up so soon?  Sissy.")
+            break
         else:
             print("Not even close, try again!")
             
